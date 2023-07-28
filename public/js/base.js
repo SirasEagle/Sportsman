b3897cc4214b13dc52e5c3d518d3e30dc5ee7705
SirasEagle <pasaguna123@gmail.com>
2023-07-28 15:14:25 +0200

    add: jquery; add: save-unit.to.workout functionality

--- public/js/base.js
+++ public/js/base.js
@@ -1,19 +1,49 @@
 // public/js/base.js
 document.addEventListener('DOMContentLoaded', function() {
    const showTableButton = document.getElementById('showTableButton');
-   const myTable = document.getElementById('myTable');
-   const reloadButton = document.getElementById('reloadButton');
+   const hiddenArea = document.getElementById('hiddenArea');
+   const saveUnitToExercise = document.getElementById('saveUnitToExercise');
+   const set1Input = document.getElementById('set1Input');
+   const set2Input = document.getElementById('set2Input');
+   const set3Input = document.getElementById('set3Input');
+   const exerciseId = document.getElementById('exerciseOption');
+   const workoutId = document.getElementById('workoutId');
 
    showTableButton.addEventListener('click', function() {
-       if (myTable.style.display === 'none') {
-           myTable.style.display = 'table';
+       if (hiddenArea.style.display === 'none') {
+           hiddenArea.style.display = 'table';
+           showTableButton.textContent = "-";
        } else {
-           myTable.style.display = 'none';
+           hiddenArea.style.display = 'none';
+           showTableButton.textContent = "+";
        }
    });
 
-   reloadButton.addEventListener('click', function() {
-       location.reload();
-   });
+    saveUnitToExercise.addEventListener('click', function() {
+        if ((set1Input.value < 0) || (set2Input.value < 0) || (set3Input.value < 0)) {
+            alert("Geben Sie eine positive Zahl ein");
+        } else {
+            // AJAX-Anfrage senden
+            $.ajax({
+                url: '/save-unit', // Die URL zu deinem Symfony-Controller
+                method: 'POST',
+                data: {
+                    set1: set1Input.value,
+                    set2: set2Input.value,
+                    set3: set3Input.value,
+                    exId: exerciseId.value,
+                    wId: workoutId.value
+                },
+                success: function(response) {
+                    // Erfolgreiche Antwort vom Server, hier kannst du entsprechende Aktionen ausführen
+                    location.reload(); // Beispiel: Seite neu laden
+                },
+                error: function(xhr, status, error) {
+                    // Bei einem Fehler kannst du hier entsprechende Fehlerbehandlung vornehmen
+                    alert('Fehler beim Speichern der Daten.' + exerciseId.value + ", " + set1Input.value);
+                }
+            });
+        }
+    });
 });
 
