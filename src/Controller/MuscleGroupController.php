af72c777ed603958d76e3160683b53845266dc75
SirasEagle <pasaguna123@gmail.com>
2025-04-23 12:13:03 +0200

    add: controller and first draft of index view for MuscleGroup

---
+++ src/Controller/MuscleGroupController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\MuscleGroup;
+use App\Form\MuscleGroupNewType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MuscleGroupController extends AbstractController
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/musclegroup', name: 'index_muscle_group')]
+    public function index(): Response
+    {
+        return $this->render('muscle_group/index.html.twig', [
+            'controller_name' => 'MuscleGroupController',
+        ]);
+    }
+
+    #[Route('/musclegroup/new', name: 'new_muscle_group')]
+    public function new(Request $request): Response
+    {
+        $muscleGroup = new MuscleGroup();
+        $form = $this->createForm(MuscleGroupNewType::class, $muscleGroup);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Werte aus dem Formular holen und in das Exercise-Objekt schreiben
+            $muscleGroup = $form->getData();
+
+            // Das Exercise-Objekt in der Datenbank persistieren
+            $this->entityManager->persist($muscleGroup);
+            $this->entityManager->flush();
+
+            // Umleiten zu einer anderen Route, z. B. zur Detailansicht des neuen Exercises
+            return $this->redirectToRoute('index_muscle_group');
+        }
+
+        return $this->render('muscle_group/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
