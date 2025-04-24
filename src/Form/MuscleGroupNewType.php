dbd0cc39429d92a5347d3747f098ddaf896989a6
SirasEagle <pasaguna123@gmail.com>
2025-04-24 20:45:44 +0200

    add: styling for muscle group form

--- src/Form/MuscleGroupNewType.php
+++ src/Form/MuscleGroupNewType.php
@@ -3,9 +3,11 @@
 namespace App\Form;
 
 use App\Entity\MuscleGroup;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MuscleGroupNewType extends AbstractType
@@ -13,11 +15,20 @@ class MuscleGroupNewType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('term')
-            ->add('description')
+            ->add('term', TextType::class, [
+                'label' => 'Bezeichnung:',
+                'attr' => ['class' => 'abc'],
+                'row_attr' => ['class' => 'mg-add-rows'],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Beschreibung:',
+                'attr' => ['class' => 'abc'],
+                'row_attr' => ['class' => 'mg-add-rows'],
+            ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Speichern',
                 'attr' => ['class' => 'abc'],
+                'row_attr' => ['class' => 'mg-add-rows'],
             ])
         ;
     }
