389ef930d5f622dd599231d92621a381b439f95d
SirasEagle <pasaguna123@gmail.com>
2025-04-25 11:34:29 +0200

    add: styling to new worjout + checkboxes

--- src/Form/WorkoutNewType.php
+++ src/Form/WorkoutNewType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class WorkoutNewType extends AbstractType
 {
@@ -19,12 +20,18 @@ class WorkoutNewType extends AbstractType
             ->add('date', DateType::class, [
                 'widget' => 'single_text', // Datepicker anzeigen
                 'data' => new \DateTime(), // Hier wird das aktuelle Datum eingestellt
+                'row_attr' => ['class' => 'mg-add-rows'],
             ])
-            ->add('info')
+            ->add('info', TextType::class, [
+                'label' => 'Information',
+                'required' => false,
+                'attr' => ['class' => 'abc'],
+                'row_attr' => ['class' => 'mg-add-rows'],
+            ])
             ->add('isReal', CheckboxType::class, [
                 'label' => 'Is it real?',
                 'required' => false,
-                'attr' => ['class' => 'abc'],
+                'row_attr' => ['class' => 'mg-add-check'],
             ])
             ->add('user', ChoiceType::class, [
                 'choices' => [
@@ -34,11 +41,13 @@ class WorkoutNewType extends AbstractType
                 'expanded' => true, // Anzeigen als Radio-Buttons
                 'label' => 'Choose user:',
                 'attr' => ['class' => 'abc'],
+                'row_attr' => ['class' => 'mg-add-rows'],
                 'mapped' => false
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Speichern',
                 'attr' => ['class' => 'abc'],
+                'row_attr' => ['class' => 'mg-add-rows'],
             ])
         ;
     }
