a93ba0c4ed2befb5616ec1b4cd47ade234b7c049
SirasEagle <pasaguna123@gmail.com>
2025-05-29 15:39:21 +0200

    fix: doctrine handling of db.portion; was mistaken with keyword

--- src/Entity/Portion.php
+++ src/Entity/Portion.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PortionRepository::class)]
+#[ORM\Table(name: '`portion`')]
 class Portion
 {
     #[ORM\Id]
